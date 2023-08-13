fff8133f4d0e5a68198f7bd0f86a78986c93944c
DavidGonzalez7 <davidgonzalez22150@gmail.com>
2023-08-13 18:23:19 -0300

    finalizados

--- eventos.js
+++ eventos.js
@@ -1,13 +1,7 @@
-// Obtenemos una referencia al div y al botón
-const divBoton = document.getElementById("botonDiv");
-const boton = document.getElementById("saludarButton");
-
-// Agregamos el manejador de eventos al div
-divBoton.addEventListener("click", function() {
-    alert("Hola! Soy el div");
-});
-
-// Evitamos que el evento del botón se propague al div
-boton.addEventListener("click", function(event) {
-    event.stopPropagation();
+document.addEventListener("DOMContentLoaded", function() {
+    const divBoton = document.getElementById("buttonContainer");
+    
+    divBoton.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
 });
